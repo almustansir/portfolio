77b410bc0e46be6375a1b346b513e380bac4a62a
Samit_08 <almustansir465@gmail.com>
2022-10-14 15:17:07 +0600

    upgraded to tsx

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -5,27 +5,33 @@ import { AiFillFacebook, AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import samit from "../public/pfp.jpeg";
 import code from "../public/code.png";
 import design from "../public/design.png";
-import web1 from "../public/web1.png"
-import web2 from "../public/web2.png"
-import web3 from "../public/web3.png"
-import web4 from "../public/web4.png"
-import web5 from "../public/web5.png"
-import web6 from "../public/web6.png"
+import web1 from "../public/web1.png";
+import web2 from "../public/web2.png";
+import web3 from "../public/web3.png";
+import web4 from "../public/web4.png";
+import web5 from "../public/web5.png";
+import web6 from "../public/web6.png";
+import { useState } from "react";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+
   return (
-    <div>
+    <div className={darkMode ? "dark" : ""}>
       <Head>
         <title>Samit's Portfolio</title>
       </Head>
 
-      <main className="bg-white px-10 md:px-20 lg:px-40">
+      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between">
+          <nav className="py-10 mb-12 flex justify-between dark:text-gray-300">
             <h1 className=" text-xl font-burtons">Samit_08</h1>
             <ul className="flex items-center">
-              <li className=" text-2xl">
-                <BsFillMoonStarsFill />
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className="cursor-pointer text-2xl"
+                />
               </li>
               <li>
                 <a
@@ -38,16 +44,16 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <div className="text-center p-10">
+          <div className="text-center p-10 ">
             <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
               Al Mustansir Samit
             </h2>
-            <h3 className="text-2xl py-2 md:text-3xl">Developer & Designer</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto">
+            <h3 className="text-2xl py-2 md:text-3xl dark:text-gray-300">Developer & Designer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-gray-300">
               Freelancer providing services for programming and design content
               needs. Join me below and let's build something awesome!
             </p>
-            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-300">
               <a href="https://www.linkedin.com/in/al-mustansir-samit-789464134">
                 <AiFillLinkedin />
               </a>
@@ -64,10 +70,10 @@ export default function Home() {
           </div>
         </section>
 
-        <section>
+        <section className="dark:text-gray">
           <div>
-            <h3 className="text-3xl py-1">Services I offer</h3>
-            <p>
+            <h3 className="text-3xl py-1 dark:text-gray-300">Services I offer</h3>
+            <p className="dark:text-gray-300">
               I am available to do remote work for{" "}
               <span className=" text-teal-500">NEXT.js</span>,{" "}
               <span className=" text-teal-500">Vue.js</span>,{" "}
@@ -77,8 +83,8 @@ export default function Home() {
               range of services depending on requirement.
             </p>
           </div>
-          <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 my-10 rounded-xl">
+          <div className="lg:flex lg:justify-center gap-40 ">
+            <div className="text-center shadow-lg p-10 my-10 rounded-xl dark:bg-gray-300">
               <Image src={code} width={100} height={100} />
               <h3 className=" text-lg font-medium pt-8 pb-2">Code</h3>
               <p className="py-2">
@@ -91,10 +97,10 @@ export default function Home() {
               <p className="py-1 text-gray-800">SQL</p>
               <p className="py-1 text-gray-800">Tailwind</p>
             </div>
-            <div className="text-center shadow-lg p-10 my-10 rounded-xl">
+            <div className="text-center shadow-lg p-10 my-10 rounded-xl dark:bg-gray-300">
               <Image src={design} width={100} height={100} />
               <h3 className=" text-lg font-medium pt-8 pb-2">
-                Beautiful Designs
+                Designs
               </h3>
               <p className="py-2">
                 Making elegant designs suited for your design theory
@@ -107,26 +113,62 @@ export default function Home() {
         </section>
         <section>
           <div>
-            <h3 className="text-3xl py-1">Portfolio</h3>
+            <h3 className="text-3xl py-1 dark:text-gray-300">Portfolio</h3>
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1 ">
-              <Image className="rounded-lg object-cover" src={web1} width={'100%'} height={'100%'} layout="responsive" />
+              <Image
+                className="rounded-lg object-cover"
+                src={web1}
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
             </div>
             <div className="basis-1/3 flex-1 ">
-              <Image className="rounded-lg object-cover" src={web2} width={'100%'} height={'100%'} layout="responsive" />
+              <Image
+                className="rounded-lg object-cover"
+                src={web2}
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
             </div>
             <div className="basis-1/3 flex-1 ">
-              <Image className="rounded-lg object-cover" src={web3} width={'100%'} height={'100%'} layout="responsive" />
+              <Image
+                className="rounded-lg object-cover"
+                src={web3}
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
             </div>
             <div className="basis-1/3 flex-1 ">
-              <Image className="rounded-lg object-cover" src={web4} width={'100%'} height={'100%'} layout="responsive" />
+              <Image
+                className="rounded-lg object-cover"
+                src={web4}
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
             </div>
             <div className="basis-1/3 flex-1 ">
-              <Image className="rounded-lg object-cover" src={web5} width={'100%'} height={'100%'} layout="responsive" />
+              <Image
+                className="rounded-lg object-cover"
+                src={web5}
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
             </div>
             <div className="basis-1/3 flex-1 ">
-              <Image className="rounded-lg object-cover" src={web6} width={'100%'} height={'100%'} layout="responsive" />
+              <Image
+                className="rounded-lg object-cover"
+                src={web6}
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
             </div>
           </div>
         </section>
