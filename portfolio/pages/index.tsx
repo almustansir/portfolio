2c183231c5c6d42c6b6bf182df827e4f8402ba16
Samit_08 <almustansir465@gmail.com>
2022-10-17 12:01:00 -0700

    new tab bug fixed.

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -164,13 +164,13 @@ export default function Home() {
                 <div className="portfolio_overlay_button">
                   <a
                     className="flex bg-gradient-to-r from-cyan-500 to-teal-500 text-white py-2 px-4 rounded-md mb-5"
-                    href="https://portfolio-almustansir.vercel.app/" target="_blank"
+                    href="https://portfolio-almustansir.vercel.app/" target="_blank" rel="noopener noreferrer"
                   >
                     Visit Website
                   </a>
                   <a
                     className=" bg-gradient-to-r from-cyan-500 to-teal-500 text-white py-2 px-4 rounded-md"
-                    href="https://github.com/almustansir/portfolio" target="_blank"
+                    href="https://github.com/almustansir/portfolio" target="_blank" rel="noopener noreferrer"
                   >
                     Checkout Git
                   </a>
